a8656b5bf75d304ff3239cef25305b9b151928d9
Martini <joe_martini@msn.com>
2014-10-16 15:46:53 +0800

    Initial Commit

---
+++ AddrManager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AddrManager
+//
+//  Created by Martini Wang on 14/10/16.
+//  Copyright (c) 2014年 Martini Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
